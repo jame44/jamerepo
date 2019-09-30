297768bac05b61de8f0b3d1c37973b641de650e3
jame44 <39670724+jame44@users.noreply.github.com>
2019-10-01 02:15:55 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,3 +1,4 @@
+Start-Transcript -path test.txt -append
 echo "I am run!"
 ls
 echo "Running step 1"
@@ -7,6 +8,7 @@ echo "Running step 2"
 
 
 echo "Running step 3"
+
 [IO.File]::ReadAllText(tratata.txt)
 
 echo "Running step 4"
