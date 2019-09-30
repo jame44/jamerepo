52b064e28fe00e0d5ea2ad26efcbb337b2857575
jame44 <39670724+jame44@users.noreply.github.com>
2019-09-30 23:15:48 +0300

    Update downloader.ps1

--- tools/downloader.ps1
+++ tools/downloader.ps1
@@ -6,7 +6,7 @@ Param(
 #Bootstrapping agent in case we need it
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$password = $password | ConvertTo-SecureString -asPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential($username,$password)
+$pair = "${username}:${password}"
+$cred = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair)
 
 Invoke-WebRequest $Url -Outfile log.txt -Credential $cred
